f9de26e70d370e828373dce009c0ffa5cc657188
Elik <nukhov_eg@mail.ru>
2021-05-28 00:07:24 +0300

    Added alerts

--- Log in app/ViewController.swift
+++ Log in app/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var userNameTextFiled: UITextField!
+    @IBOutlet weak var passwordTextFiled: UITextField!
+    
+    
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var forgotUserNameButton: UIButton!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func forgotUserNameButton(_ sender: UIButton) {
+        
+        let userNameAlert = UIAlertController(title: "Внимание!", message: "Твой ник - Elnur", preferredStyle: .alert)
+        let userNameAlertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        userNameAlert.addAction(userNameAlertAction)
+        present(userNameAlert, animated: true, completion: nil)
+    }
 
+    @IBAction func forgotPasswordButton(_ sender: UIButton) {
+        
+        let passwordAlert = UIAlertController(title: "Внимание!", message: "Твой пароль - Batman ", preferredStyle: .alert)
+        let passwordAlertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        passwordAlert.addAction(passwordAlertAction)
+        present(passwordAlert, animated: true, completion: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dwc = segue.destination as? WelcomeViewController else  { return }
+        dwc.welcomeMessage = userNameTextFiled.text
+        
+    }
 }
-
